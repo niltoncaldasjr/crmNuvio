52a5caef914930c1cafd176d666a92b0c2e19eaf
Adelson <Adelson@Adelson-Note>
2015-06-24 18:08:03 -0400

    Classes ContaBanco

---
+++ model/contabanco/ContaBancoDAO.php
@@ -0,0 +1,128 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/crmNuvio/" . 'model/contabanco/ContaBanco.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . "/crmNuvio/" . 'model/banco/Banco.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . "/crmNuvio/" . 'control/BancoControl.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . "/crmNuvio/" . 'model/empresa/Empresa.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . "/crmNuvio/" . 'control/EmpresaControl.php';
+
+class ContaBancoDAO{
+	/*-- Criando atributos da class --*/
+	private $con;
+	private $sql;
+	private $objContaBanco;
+	private $listaContaBanco = array();
+	
+	function __construct($con){
+		$this->con = $con;
+	}
+	
+	/*-- Metodo Cadastrar --*/
+	function cadastrar(ContaBanco $objContaBanco){
+		$this->sql = sprintf("INSERT INTO contabanco (agencia, digitoAgencia, numeroConta, digitoConta, numeroCarteira, numeroConvenio, nomeContato, telefoneContato, login, datasis, idbanco, idempresa) 
+				VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d)",
+				mysqli_real_escape_string( $this->con, $objContaBanco->getAgencia() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getdigitoAgencia() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNumeroConta() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getDigitoConta() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNumeroCarteira() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNumeroConvenio() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNomeContato() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getTelefoneContato() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getLogin() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getDatasis() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getObjBanco()->getId() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getObjEmpresa()->getId() ) );
+				
+		if(!mysqli_query($this->con, $this->sql)){
+			die('[ERRO] Cadastro: '.mysqli_error($this->con));
+		}
+		return $this->objContaBanco = $objContaBanco;
+	}
+	
+	/*-- Metodo Atualizar --*/
+	function atualizar(ContaBanco $objContaBanco){
+		$this->sql = sprintf("UPDATE contabanco SET agencia = '%s', digitoAgencia = '%s', numeroConta ='%s', digitoConta = '%s', numeroCarteira = '%s', numeroConvenio = '%s', nomeContato = '%s', telefoneContato = '%s', login = '%s', datasis = '%s', idbanco = %d, idempresa = %d WHERE id = %d",
+				mysqli_real_escape_string( $this->con, $objContaBanco->getAgencia() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getdigitoAgencia() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNumeroConta() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getDigitoConta() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNumeroCarteira() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNumeroConvenio() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getNomeContato() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getTelefoneContato() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getLogin() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getDatasis() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getObjBanco()->getId() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getObjEmpresa()->getId() ),
+				mysqli_real_escape_string( $this->con, $objContaBanco->getId() ) );
+		if(!mysqli_query($this->con, $this->sql)){
+			die('[ERRO]: '.mysqli_error($this->con));
+		}
+		return $this->objContaBanco = $objContaBanco;
+	}
+	
+	/*-- Deletar --*/
+	function deletar(ContaBanco $objContaBanco){
+		$this->sql = sprintf("DELETE FROM contabanco WHERE id = %d",
+				mysqli_real_escape_string( $this->con, $objContaBanco->getId() ) );
+		if(!mysqli_query($this->con, $this->sql)){
+			die('[ERRO]: '.mysqli_error($this->con));
+		}
+		return $this->objContaBanco = $objContaBanco;
+	}
+	
+	/*-- Buscar por ID --*/
+	function buscarPorID(ContaBanco $objContaBanco){
+		$this->sql = sprintf("SELECT * FROM contabanco WHERE id = %d",
+				mysqli_real_escape_string( $this->con, $objContaBanco->getId() ) );
+		
+		$resultSet = mysqli_query($this->con, $this->sql);
+		if(!$resultSet){
+			die('[ERRO]: '.mysqli_error($this->con));
+		}
+		while($row = mysqli_fetch_object($resultSet)){
+			$objBanco = new Banco();
+			$objBanco->setId($row->idbanco);
+			$objBancoControl = new BancoControl($objBanco);
+			$objBanco = $objBancoControl->buscarPorID();
+			
+			$objEmpresa = new Empresa();
+			$objEmpresa->setId($row->idempresa);
+			$objEmpresaControl = new EmpresaControl($objEmpresa);
+			$objEmpresa = $objEmpresaControl->buscarPorId();
+			
+			$this->objContaBanco = new ContaBanco($row->id, $row->agencia, $row->digitoAgencia, $row->numeroConta, $row->digitoConta, $row->numeroCarteira, $row->numeroConvenio, $row->nomeContato, $row->telefoneContato, $row->login, $row->datasis, $objBanco, $objEmpresa); 
+		}
+		
+		return $this->objContaBanco;
+	}
+	
+	/*-- Listar Todos --*/
+	function listarTodos(ContaBanco $objContaBanco){
+		$this->sql = "SELECT * FROM contabanco";
+		$resultSet = mysqli_query($this->con, $this->sql);
+		if(!$resultSet){
+			die('[ERRO]: '.mysqli_error($this->con));
+		}
+		while($row = mysqli_fetch_object($resultSet)){
+			$objBanco = new Banco();
+			$objBanco->setId($row->idbanco);
+			$objBancoControl = new BancoControl($objBanco);
+			$objBanco = $objBancoControl->buscarPorID();
+				
+			$objEmpresa = new Empresa();
+			$objEmpresa->setId($row->idempresa);
+			$objEmpresaControl = new EmpresaControl($objEmpresa);
+			$objEmpresa = $objEmpresaControl->buscarPorId();
+				
+			$this->objContaBanco = new ContaBanco($row->id, $row->agencia, $row->digitoAgencia, $row->numeroConta, $row->digitoConta, $row->numeroCarteira, $row->numeroConvenio, $row->nomeContato, $row->telefoneContato, $row->login, $row->datasis, $objBanco, $objEmpresa); 
+				
+			array_push($this->listaContaBanco, $this->objContaBanco);
+		}
+		
+		return $this->listaContaBanco;
+	}
+	
+			
+}
+?>
