5e14c5a587c6095d98d6d51c77d8d02f83122d77
Nilton Caldas Júnior <niltonbox@gmail.com>
2015-06-30 18:18:51 -0400

    Alterações em VIEW parte 3

--- view/app/view/Viewport.js
+++ view/app/view/Viewport.js
@@ -8,83 +8,25 @@
 */
 
 Ext.define('crm.view.Viewport', {
-    extend: 'Ext.container.Viewport',
-    requires:[
-        'Ext.layout.container.Fit'
-    ],
-
-   
-  layout: {
-      type: 'border'
-  },
-	
-  items: [
-		{
-			region: 'center',
-			xtype: 'panel',
-			layout: {
-		        type: 'vbox',
-		        align: 'stretch'
-		    },
-			items: [{
-		        flex: 2,
-		        xtype: 'pessoagrid',
-		        collapsible: true,
-				split: true
-		    },
-		    {
-		        xtype: 'pessoaform',
-//		        title: 'Inner Panel Two',
-		        collapsible: true,
-		        flex: 2
-		    }]
-		},
-		{
-			region: 'west',
-			xtype: 'panel',
-			split: true,
-			title: 'Filtro por Perfil',
-			width: 200,
-			bodyStyle: 'padding:15px',
-			html: '<h4>Escolha do Perfil:</h4> <br><br>[ ] - Administrador<br>[ ] - Operador<br>[ ] - Digitador<br>[ ] - Outros',
-		},
-		{
-			region: 'east',
-			xtype: 'panel',
-			title: 'Histórico do Usuário',
-			width: 400,
-			bodyStyle: 'padding:15px',
-			html: '<h4>Dados Histórico do Usuários:</h4> <br><br>1 - Fotos<br>2 - Dados Pessoais<br>3 - Logs de Acesso<br>4 - Etc.',
-			collapsible: true,
-			split: true
-		},
-		{
-			// Topo da Tela Viewport
-			region: 'north',
-			xtype: 'panel',
-			layout: {
-		        type: 'vbox',
-		        align: 'stretch'
-		    },
-			height: 100,			
-			items: [
-			        {
-			        	height: 70,
-			        	html: '<table><tr><td><img src="/git/akto/cau/resources/images/logo.jpg"></td><td><h3>Controle e Autenticação de Usuário</h3></td></tr></table> '
-			        },
-			        {
-			        	xtype:'menuprincipal',
-			        	style: 'background-color: #990000;'
-			        }
-			]	
-		},
-		{
-			region: 'south',
-			xtype: 'panel',
-			split: true,
-			bodyStyle: 'padding:5px',
-			html: 'Rodapé do Sistema',
-		}
-	]    
+    extend: 'Ext.Viewport',    
+    layout: 'fit',
     
+    requires: [
+        'crm.view.pais.PaisGrid',
+        'crm.view.pais.PaisForm'
+    ],
+    
+    initComponent: function() {
+        var me = this;
+        
+        Ext.apply(me, {
+            items: [
+                {
+                    xtype: 'paisgrid'
+                }
+            ]
+        });
+                
+        me.callParent(arguments);
+    }
 });
