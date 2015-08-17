2c274787d5222a1f3be530f4c9e351a8ec8a5bbf
Adelson <Adelson@Adelson-Note>
2015-08-17 14:54:40 -0400

    Exclusão da Model EmpresaUsuário(Desnecessária)

--- view/app/model/EmpresaUsuario.js
+++
@@ -1,9 +0,0 @@
-Ext.define('crm.model.EmpresaUsuario',{
-	extend: 'Ext.data.Model',
-	
-	fields: [
-	   {name: 'idempresa', 		type: 'int'},
-	   {name: 'CNPJ', 			type: 'string'},
-	   {name: 'datacadastro', 	type: 'date', dateFormat: 'c'}
-	]
-});
